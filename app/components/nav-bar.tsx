5115d8c7c5b7d8aebb068bb4434339c4fef9b3c5
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-21 18:45:54 +0900

    add: navコンポーネント

---
+++ app/components/nav-bar.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function NavBar() {
+  return (
+    <header className="bg-gray-800 p-4">
+      <nav className="space-x-4"></nav>
+    </header>
+  )
+}
