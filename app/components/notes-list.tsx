7b463e822a6860dd19897693e5232ee00a3fd4bb
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-26 12:03:10 +0900

    modify: fetchしたデータを表示

--- app/components/notes-list.tsx
+++ app/components/notes-list.tsx
@@ -20,5 +20,22 @@ async function fetchNotes() {
 
 export default async function NotesList() {
   const notes = await fetchNotes()
-  return <div></div>
+  return (
+    <div>
+      <p className=" my-4 pb-3 text-xl font-medium underline underline-offset-4">
+        Notes
+      </p>
+      <ul className="m-3">
+        {notes.map((note) => (
+          <li key={note.id}>
+            <p> {note.title}</p>
+            <p>
+              <strong className="mr-3">Created at:</strong>
+              {note && format(new Date(note.created_at), 'yyyy-MM-dd HH:mm:ss')}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
