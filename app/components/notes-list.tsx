a44aab71b1fec60d7313babb51f2db67d5a59cba
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-26 11:11:05 +0900

    modify: Database.typesから必要な方だけ取り出す

--- app/components/notes-list.tsx
+++ app/components/notes-list.tsx
@@ -1,3 +1,8 @@
+import type { Database } from '../../database.types'
+import { format } from 'date-fns'
+
+type Note = Database['public']['Tables']['notes']['Row']
+
 export default async function NotesList() {
   return <div></div>
 }
