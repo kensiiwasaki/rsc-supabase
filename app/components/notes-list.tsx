ba94eac9378e512d2c2d304154d62f9102d81d33
kensiiwasaki <kensiiwasaki@gmail.com>
2023-04-26 11:14:55 +0900

    modify: fetch処理追加(意図的に２秒間遅延)

--- app/components/notes-list.tsx
+++ app/components/notes-list.tsx
@@ -3,6 +3,16 @@ import { format } from 'date-fns'
 
 type Note = Database['public']['Tables']['notes']['Row']
 
+async function fetchNotes() {
+  // 挙動がわかりやすいように意図的に２秒間遅延
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  const res = await fetch(`${process.env.url}/reset/v1/notes?select=*`, {
+    headers: new Headers({
+      apiKey: process.env.apiKey as string,
+    }),
+  })
+}
+
 export default async function NotesList() {
   return <div></div>
 }
